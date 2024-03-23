3f97996618c8f5018e2e9f753c4060d5a0d0880a
Jose-AE <107892696+Jose-AE@users.noreply.github.com>
2024-03-23 01:40:37 -0600

    Implement basic AST defenitions

--- src/ast.ts
+++ src/ast.ts
@@ -1,5 +1,43 @@
-export enum NodeType {
-  PROGRAM,
-  NUMERIC_LITERAL,
-  ID,
+export type Literal = string | number | boolean;
+export type Expression = Literal | BinaryExpression | Identifier;
+export type Statment = VariableDeclaration | IfStatement | WhileStatement;
+
+export interface Program {
+  body: BlockStatement;
+}
+
+export interface BlockStatement {
+  body: Statment[];
+}
+
+export interface IfStatement {
+  condition: Condition;
+  ifTrue: BlockStatement;
+  ifFalse: BlockStatement | IfStatement;
+}
+
+export interface WhileStatement {
+  condition: Condition;
+  body: BlockStatement;
+}
+
+export interface Condition {
+  symbol: string;
+  left: Expression;
+  rigth: Expression;
+}
+
+export interface VariableDeclaration {
+  id: Identifier;
+  value: Expression;
+}
+
+export interface BinaryExpression {
+  left: Expression;
+  right: Expression;
+  operator: string;
+}
+
+export interface Identifier {
+  value: string;
 }
